c1fa0ebced139cd018c70e4fb2d9a22b3500b87b
zshaheen <zshaheen93@gmail.com>
2017-07-20 12:39:34 -0700

    demo on single laptop

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+from dask.distributed import Client
+
+def square(x):
+    return x**2
+
+# start a schduler and client on the current machine
+client = Client()
+
+a = client.submit(square, 10) # returns a single Future object
+b = client.map(square, range(10)) # returns a list of Future objects
+
+# Getting the results
+print a.result()
+print client.gather(b) # returns [f.result() for f in b]
